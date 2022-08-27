7ca93e88bf0bed0c5a5f2ee9863d4eabc7bbde0f
Artem Derbin <artemsdft@mail.ru>
2022-08-27 12:58:39 +0300

    Initial Commit

---
+++ Home Work 17 Apple Music/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Home Work 17 Apple Music
+//
+//  Created by Артем Дербин on 27.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Home Work 17 Apple Music/Home_Work_17_Apple_MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  Home_Work_17_Apple_MusicApp.swift
+//  Home Work 17 Apple Music
+//
+//  Created by Артем Дербин on 27.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct Home_Work_17_Apple_MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
