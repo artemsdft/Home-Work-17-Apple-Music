4bb46c740e26206eb539b880878f1e45604af75f
Artem Derbin <artemsdft@mail.ru>
2022-08-27 13:14:27 +0300

    added navigationView

---
+++ Home Work 17 Apple Music/NavigationView.swift
@@ -0,0 +1,44 @@
+//
+//  NavigationView.swift
+//  Home Work 17 Apple Music
+//
+//  Created by Артем Дербин on 27.08.2022.
+//
+
+import SwiftUI
+
+struct NavigationView: View {
+    @State var isEditMode: EditMode = .active
+    @State var selectionScreen = true
+    var body: some View {
+        NavigationView {
+            VStack {
+                if selectionScreen {
+                    LibraryView()
+                } else {
+                    ListView()
+                }
+            }
+            .navigationTitle("Медиатека")
+            .environment(\.editMode, $isEditMode)
+            .toolbar(content: {
+                Button(action: {
+                    selectionScreen.toggle()
+                }, label: {
+                    if selectionScreen {
+                        Text("править")
+                    } else {
+                        Text("готово")
+                    }
+                })
+            })
+        }
+        .navigationBarBackButtonHidden(false)
+    }
+}
+
+struct NavigationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView()
+    }
+}
